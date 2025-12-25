40bc6846ebba8fab5e0aeda531de2aec2b3e7187
A11ert <amangel.com@gmail.com>
2025-12-25 05:22:33 +0500

    UniHospitalProject

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Patient p1 = new Patient("071107554554", "Yernaz", 18, 15000);
+
+    }
+}
